f586e270d56b8f157248a301e324f387f02a6363
PavanSaxena <meetpavansaxena@gmail.com>
2025-03-24 09:56:55 +0530

    added

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const Tile = (person) => {
+const Tile = ({ image, name, role, email, slots }) => {
   return (
-    <div className='flex'>
-    <div className="bg-white shadow-xl rounded-lg items-center my-10 mx-10 w-full">
-        <img src={person.image} alt="" className="rounded-t-md object-cover w-full" />
-        <div className="pb-4 flex flex-col px-5 py-2 text-xl">
-            <h2 className="font-semibold text-gray-900">{person.name}</h2>
-            <p className="text-gray-600">{person.role}</p>
-            <p className="text-gray-600">{person.email}</p>
-            <div className='flex flex-col items-center p-2'>
-                <button className='bg-sky-900 hover:bg-blue-700 text-white px-7 py-1 my-1 rounded-md'>View Information</button>
-                <button className='bg-sky-900 hover:bg-blue-700 text-white px-6 py-1 mt-1 rounded-md'>Request Guideship</button>
-                <p className='pt-2'>{person.slots} slots remaining</p>
-            </div>
+    <div className="flex">
+      <div className="bg-white shadow-xl rounded-lg my-10 mx-10 w-full">
+        <img src={image} alt={name} className="rounded-t-md object-cover w-full" />
+        <div className="pb-4 flex flex-col px-5 py-2 text-md">
+          <h2 className="font-semibold text-gray-900">{name}</h2>
+          <p className="text-gray-600">{role}</p>
+          <p className="text-gray-600">{email}</p>
+          <div className="flex flex-col items-center p-2">
+            <button className="bg-sky-900 hover:bg-blue-700 text-white px-7 py-1 my-1 rounded-md">
+              View Information
+            </button>
+            <button className="bg-sky-900 hover:bg-blue-700 text-white px-6 py-1 mt-1 rounded-md">
+              Request Guideship
+            </button>
+            <p className="pt-2">{slots} slots remaining</p>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Tile
+export default Tile;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,199 +1,137 @@
-import React from 'react'
-import Tile from '../components/Tile'
-const people = [
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20210615110838000686.jpg",
-    name: "Dr. Sandesh B J",
-    role: "Chairperson, Professor",
-    email: "sandesh_bj@pes.edu",
-    slots: 4,
-  },
-  {
-    name: "Dr. Arti Arya",
-    role: "Professor",
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230920113626192596_Arti_Arya.jpg",
-    email: "artiarya@pes.edu",
-    slots: 4,
-  },
-  {
-    name: "Deepa S",
-    role: "Professor",
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230925033325983162_deepa_s_1.jpg",
-    email: "deepa.s@pes.edu",
-    slots: 4,
-  },
-  {
-    name: "Dr. Farida Begam M",
-    role: "Professor",
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20240226084029459630_Dr_M_Farida_Begam_1.jpg",
-    email: "farida.begam@pes.edu",
-    slots: 4,
-  },
-  {
-    name: "Dr. Pooja Agarwal",
-    role: "Professor",
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230920111907588323_Pooja_Agarwal.jpg",
-    email: "poojaagarwal@pes.edu",
-    slots: 4,
-  },
-  {
-    name: "Dr. Prashanth Athri",
-    role: "Professor",
-    image: "https://staff.pes.edu/static/images/def.png",
-    email: "athri@pes.edu",
-    slots: 4,
-  },
-  {
-    name: "Dr. Sarasvathi V",
-    role: "Professor",
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230920081456706599_Sarasvathi_V.jpg",
-    email: "sarsvathiv@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921062645765137_Animesh_Giri.jpg",
-    name: "Animesh Giri",
-    role: "Associate Professor",
-    email: "animeshgiri@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921065504695350_Dr._Chandrashekhar_P_Chavan.jpg",
-    name: "Dr. Chandrashekhar P Chavan",
-    role: "Associate Professor",
-    email: "cpchavan@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/image_20230704055327_72fda189.jpg",
-    name: "Dr. Charu Kathuria",
-    role: "Associate Professor",
-    email: "charu.kathuria@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921063510563513_Dr._Clara_Kanmani_A.jpg",
-    name: "Dr. Clara Kanmani A",
-    role: "Associate Professor",
-    email: "clarakanmani.a@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921060932209849_Dr._Geetha_D.jpg",
-    name: "Dr. Geetha  D",
-    role: "Associate Professor",
-    email: "geethadayalan@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230919114118919410_Jeny_Jijo.jpg",
-    name: "Jeny Jijo",
-    role: "Associate Professor",
-    email: "jenyjijo@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921063651137639_Dr._Kamatchi_Priya_L.jpg",
-    name: "Dr. Kamatchi Priya L",
-    role: "Associate Professor",
-    email: "priyal@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20250310191720253369_Dr.KavithaP.jpg",
-    name: "Dr. Kavitha P",
-    role: "Associate Professor",
-    email: "pkavitha@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20250305125327976298_manju.jpg",
-    name: "Dr. Manju",
-    role: "Associate Professor",
-    email: "manju.nunia@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20241104134411620925_staff041124.jpg",
-    name: "Dr. Richa Sharma",
-    role: "Associate Professor",
-    email: "richasharma@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230919113220821269_Saritha.jpg",
-    name: "Dr. Saritha",
-    role: "Associate Professor",
-    email: "saritha.k@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20241104132139605824_New_Project.jpg",
-    name: "Dr. Sharavana K",
-    role: "Associate Professor",
-    email: "sharavana.kk@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20240329050040046518_Surbhi.jpg",
-    name: "Surbhi Choudhary",
-    role: "Assistant Professor",
-    email: "surbhichoudhary@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20240807031747019295_New_Project.jpg",
-    name: "Swathi Priya N",
-    role: "Assistant Professor",
-    email: "swathipriya.n@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20240119095639868016_Pavithra_S_1.jpg",
-    name: "Pavithra S",
-    role: "Assistant Professor",
-    email: "pavithra.s@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20250311180213852108_Dr_R_Nandhi_Kesavan.jpg",
-    name: "Dr. R Nandhi Kesavan",
-    role: "Assistant Professor",
-    email: "Nandhi.Kesavan@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921081828614623_Sheela_Devi_M.jpg",
-    name: "Sheela Devi M",
-    role: "Assistant Professor",
-    email: "sheelam@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230921082603629580_Shilpa_S.jpg",
-    name: "Shilpa S",
-    role: "Assistant Professor",
-    email: "shilpas@pes.edu",
-    slots: 4,
-  },
-  {
-    image: "https://storage.googleapis.com/chrome-ability-447306-j9.appspot.com/staff/Uploads/20230919120015326284_Shruthi_L.jpg",
-    name: "Shruthi L",
-    role: "Assistant Professor",
-    email: "shruthil@pes.edu",
-    slots: 4,
-  },
-];
+import React, { useState, useEffect } from 'react';
+import Tile from '../components/Tile';
+import { FaCaretRight, FaCaretLeft } from "react-icons/fa";
+import facultyData from "../assets/cleaned_updated_faculty_details.json";
+const groupByDomain = (facultyList) => {
+  const domainMap = {}
+  facultyList.forEach((faculty)=>{
+    Object.keys(faculty).forEach((key)=>{
+      if(key.startsWith("Domain-") && faculty[key]){
+        if(!domainMap[faculty[key]]){
+          domainMap[faculty[key]] = []
+        }
+        domainMap[faculty[key]].push(faculty)
+      }
+    })
+  })
+  return domainMap
+}
 const Home = () => {
+  const sortedFaculty = groupByDomain(facultyData);
   return (
     <div className="min-h-screen bg-gray-100 p-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
-        {people.map((person, index) => {
-          return <Tile key={index} {...person} />
-        })}
+      {Object.entries(sortedFaculty).map(([domain, facultyList]) => {
+        return (<div key={domain} className="mb-10">
+          <h2 className="text-3xl font-bold text-blue-950 mb-4">{domain}</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {facultyList.map((faculty, index) => (
+              <Tile
+                key={index}
+                image={faculty["Image URL"]}
+                name={faculty["Name"]}
+                role={faculty["Designation"]}
+                email={faculty["Email"]}
+                slots={4}
+              />
+            ))}
+          </div>
+        </div>)
+      })}
+      <div className='flex w-full justify-center gap-2'>
+        <button className='bg-blue-950 text-white text-2xl rounded-md p-2 disabled:opacity-50'><FaCaretLeft/></button>
+        <button className='bg-blue-950 text-white text-2xl rounded-md p-2 disabled:opacity-50'><FaCaretRight/></button>
       </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
+// import React, { useState } from 'react';
+// import Tile from '../components/Tile';
+// import { FaCaretRight, FaCaretLeft } from "react-icons/fa";
+// import facultyData from "../assets/cleaned_updated_faculty_details.json";
+
+// const groupByDomain = (facultyList) => {
+//   const domainMap = {};
+//   facultyList.forEach((faculty) => {
+//     Object.keys(faculty).forEach((key) => {
+//       if (key.startsWith("Domain-") && faculty[key]) {
+//         if (!domainMap[faculty[key]]) {
+//           domainMap[faculty[key]] = [];
+//         }
+//         domainMap[faculty[key]].push(faculty);
+//       }
+//     });
+//   });
+//   return domainMap;
+// };
+
+// const paginateFaculty = (sortedFaculty, currentPage, tilesPerPage) => {
+//   let flatList = [];
+//   Object.entries(sortedFaculty).forEach(([domain, facultyList]) => {
+//     flatList.push({ type: "header", name: domain });
+//     flatList = [...flatList, ...facultyList];
+//   });
+
+//   const startIdx = (currentPage - 1) * tilesPerPage;
+//   const endIdx = startIdx + tilesPerPage;
+//   return flatList.slice(startIdx, endIdx);
+// };
+
+// const Home = () => {
+//   const sortedFaculty = groupByDomain(facultyData);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const tilesPerPage = 20;
+//   const totalPages = Math.ceil(Object.values(sortedFaculty).flat().length / tilesPerPage);
+//   const paginatedFaculty = paginateFaculty(sortedFaculty, currentPage, tilesPerPage);
+
+//   const handleNext = () => {
+//     if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+//   };
+
+//   const handlePrev = () => {
+//     if (currentPage > 1) setCurrentPage(currentPage - 1);
+//   };
+
+//   return (
+//     <div className="min-h-screen bg-gray-100 p-10">
+//       {paginatedFaculty.map((item, index) =>
+//         item.type === "header" ? (
+//           <h2 key={index} className="text-3xl font-bold text-blue-950 mb-4">{item.name}</h2>
+//         ) : null
+//       )}
+
+//       {/* Grid Layout for 4 Tiles Per Row */}
+//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+//         {paginatedFaculty.map(
+//           (item, index) =>
+//             item.type !== "header" && (
+//               <Tile
+//                 key={index}
+//                 image={item["Image URL"]}
+//                 name={item["Name"]}
+//                 role={item["Designation"]}
+//                 email={item["Email"]}
+//                 slots={4}
+//               />
+//             )
+//         )}
+//       </div>
+
+//       {/* Pagination Controls */}
+//       <div className="flex w-full justify-center gap-4 mt-6">
+//         <button onClick={handlePrev} disabled={currentPage === 1} className="bg-blue-950 text-white text-2xl rounded-md p-2 disabled:opacity-50">
+//           <FaCaretLeft />
+//         </button>
+//         <span className="text-xl font-semibold text-gray-800">
+//           Page {currentPage} of {totalPages}
+//         </span>
+//         <button onClick={handleNext} disabled={currentPage === totalPages} className="bg-blue-950 text-white text-2xl rounded-md p-2 disabled:opacity-50">
+//           <FaCaretRight />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
